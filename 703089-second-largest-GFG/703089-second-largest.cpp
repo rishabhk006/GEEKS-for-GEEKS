51b38017259d79653f93cac1a3ac0215c7f3bd74
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-06-29 15:02:09 +0530

    Added solution - LeetHub

---
+++ 703089-second-largest-GFG/703089-second-largest.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int getSecondLargest(vector<int> &arr) {
+        int first = -1, second = -1;
+
+        for (int x : arr) {
+            if (x > first) {
+                second = first;
+                first = x;
+            }
+            else if (x > second && x != first) {
+                second = x;
+            }
+        }
+
+        return second;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
