ff1fe15afd64627fbf3ef96d2dc7a129d786ed88
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-06-29 15:01:17 +0530

    Added solution - LeetHub

---
+++ 701889-missing-in-array-GFG/701889-missing-in-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int missingNum(vector<int>& arr) {
+        int n = arr.size() + 1;
+        long long total = 1LL * n * (n + 1) / 2;
+        long long sum = 0;
+
+        for (int x : arr)
+            sum += x;
+
+        return total - sum;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
