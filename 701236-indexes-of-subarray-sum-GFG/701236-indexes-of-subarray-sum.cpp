eeabcf6fa5ad582ec2e5ab547524dd80863372b9
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-06-29 12:52:03 +0530

    Added solution - LeetHub

---
+++ 701236-indexes-of-subarray-sum-GFG/701236-indexes-of-subarray-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    vector<int> subarraySum(vector<int> &arr, int target) {
+        int n = arr.size();
+        int sum = 0, left = 0;
+
+        for (int right = 0; right < n; right++) {
+            sum += arr[right];
+
+            while (sum > target && left <= right) {
+                sum -= arr[left];
+                left++;
+            }
+
+            if (sum == target) {
+                return {left + 1, right + 1};
+            }
+        }
+
+        return {-1};
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
