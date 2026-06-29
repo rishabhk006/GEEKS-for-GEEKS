28336ab6d2978dd5cd62160e204b1beec38bdd97
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-06-29 15:02:54 +0530

    Added solution - LeetHub

---
+++ 701215-kadanes-algorithm-GFG/701215-kadanes-algorithm.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int maxSubarraySum(vector<int> &arr) {
+        int curr = arr[0];
+        int maxi = arr[0];
+
+        for (int i = 1; i < arr.size(); i++) {
+            curr = max(arr[i], curr + arr[i]);
+            maxi = max(maxi, curr);
+        }
+
+        return maxi;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
