0a3e2a8f9f2330beb7c717cfd3077e969d146af1
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-06-30 11:50:06 +0530

    Added solution - LeetHub

---
+++ 700437-duplicates-in-a-limited-range-array-GFG/700437-duplicates-in-a-limited-range-array.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& arr) {
+        vector<int> duplicates;
+        
+        for (int i = 0; i < arr.size(); i++) {
+            int idx = abs(arr[i]) - 1;
+            
+            if (arr[idx] < 0) {
+                duplicates.push_back(idx + 1);
+            } else {
+                arr[idx] = -arr[idx];
+            }
+        }
+        
+        return duplicates;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
