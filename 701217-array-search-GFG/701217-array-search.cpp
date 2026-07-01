b6d0fb07c9738650c2a7eb768730df24b8d4a97f
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-01 11:18:40 +0530

    Added solution - LeetHub

---
+++ 701217-array-search-GFG/701217-array-search.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+class Solution {
+public:
+    int search(std::vector<int>& arr, int x) {
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
