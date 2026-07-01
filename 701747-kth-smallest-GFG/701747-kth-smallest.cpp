df4788701be865c5fc453e6e9e41c73fd2171df0
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-01 11:10:15 +0530

    Added solution - LeetHub

---
+++ 701747-kth-smallest-GFG/701747-kth-smallest.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <queue>
+
+class Solution {
+public:
+    int kthSmallest(std::vector<int>& arr, int k) {
+        std::priority_queue<int> max_heap;
+
+        for (int i = 0; i < arr.size(); i++) {
+            max_heap.push(arr[i]);
+            if (max_heap.size() > k) {
+                max_heap.pop();
+            }
+        }
+
+        return max_heap.top();
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
