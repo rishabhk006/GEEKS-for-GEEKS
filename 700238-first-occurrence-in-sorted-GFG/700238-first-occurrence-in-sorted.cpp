9cf84c9bf102b293ac7ca0377aaeb5ac04d74ef2
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-01 11:14:49 +0530

    Added solution - LeetHub

---
+++ 700238-first-occurrence-in-sorted-GFG/700238-first-occurrence-in-sorted.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+
+class Solution {
+public:
+    int firstSearch(std::vector<int>& arr, int k) {
+        int low = 0;
+        int high = arr.size() - 1;
+        int result = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == k) {
+                result = mid;     
+                high = mid - 1;   
+            } 
+            else if (arr[mid] < k) {
+                low = mid + 1;    
+            } 
+            else {
+                high = mid - 1;   
+            }
+        }
+
+        return result;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
