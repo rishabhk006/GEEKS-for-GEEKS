9c0b4441e792cf8205d58909b2ba96b78c7f6aa9
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-01 11:13:40 +0530

    Added solution - LeetHub

---
+++ 701213-rotate-array-GFG/701213-rotate-array.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+private:
+    void reverse(std::vector<int>& arr, int start, int end) {
+        while (start < end) {
+            std::swap(arr[start], arr[end]);
+            start++;
+            end--;
+        }
+    }
+
+public:
+    void rotateArr(std::vector<int>& arr, int d) {
+        int n = arr.size();
+        if (n == 0) return;
+        
+        d = d % n; 
+        if (d == 0) return;
+
+        reverse(arr, 0, d - 1);
+        reverse(arr, d, n - 1);
+        reverse(arr, 0, n - 1);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
