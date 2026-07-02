7b2d1bad37483e5b70ea4532903ee33bfbc84344
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-02 11:51:39 +0530

    Added solution - LeetHub

---
+++ 700151-quick-sort-GFG/700151-quick-sort.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void quickSort(vector<int>& arr, int low, int high) {
+        if (low < high) {
+            int pIdx = partition(arr, low, high);
+            quickSort(arr, low, pIdx - 1);
+            quickSort(arr, pIdx + 1, high);
+        }
+    }
+
+    int partition(vector<int>& arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr[i], arr[j]);
+            }
+        }
+        swap(arr[i + 1], arr[high]);
+        return i + 1;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
