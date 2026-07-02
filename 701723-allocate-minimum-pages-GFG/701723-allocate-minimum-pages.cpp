5b265319e7e63735544e3737dd470179d720e7d3
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-02 11:47:43 +0530

    Added solution - LeetHub

---
+++ 701723-allocate-minimum-pages-GFG/701723-allocate-minimum-pages.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    bool isPossible(const vector<int> &arr, int k, int max_pages) {
+        int student_count = 1;
+        int current_pages = 0;
+        
+        for (int pages : arr) {
+            if (pages > max_pages) {
+                return false;
+            }
+            if (current_pages + pages > max_pages) {
+                student_count++;
+                current_pages = pages;
+                if (student_count > k) {
+                    return false;
+                }
+            } else {
+                current_pages += pages;
+            }
+        }
+        return true;
+    }
+
+public:
+    int findPages(vector<int> &arr, int k) {
+        int n = arr.size();
+        if (n < k) {
+            return -1;
+        }
+        
+        int low = *max_element(arr.begin(), arr.end());
+        int high = accumulate(arr.begin(), arr.end(), 0);
+        int res = -1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (isPossible(arr, k, mid)) {
+                res = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return res;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
