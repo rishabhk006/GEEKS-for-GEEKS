78b979dfcb987df43dd1db942e5f79c200f124dc
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-02 11:50:18 +0530

    Added solution - LeetHub

---
+++ 702828-search-in-rotated-sorted-array-GFG/702828-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& arr, int key) {
+        int low = 0;
+        int high = arr.size() - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (arr[mid] == key) {
+                return mid;
+            }
+            
+            if (arr[low] <= arr[mid]) {
+                if (key >= arr[low] && key < arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (key > arr[mid] && key <= arr[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        
+        return -1;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
