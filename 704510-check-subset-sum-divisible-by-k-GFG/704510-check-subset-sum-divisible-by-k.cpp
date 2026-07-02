f25e6225e55cff83910929375229b8b138cba26d
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-02 11:43:08 +0530

    Added solution - LeetHub

---
+++ 704510-check-subset-sum-divisible-by-k-GFG/704510-check-subset-sum-divisible-by-k.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool divisibleByK(vector<int>& arr, int k) {
+        if (arr.size() >= k) {
+            return true;
+        }
+
+        vector<bool> dp(k, false);
+
+        for (int num : arr) {
+            vector<bool> next_dp = dp;
+            
+            int current_rem = num % k;
+            next_dp[current_rem] = true;
+
+            for (int r = 0; r < k; r++) {
+                if (dp[r]) {
+                    next_dp[(r + current_rem) % k] = true;
+                }
+            }
+
+            dp = next_dp;
+
+            if (dp[0]) {
+                return true;
+            }
+        }
+
+        return dp[0];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
