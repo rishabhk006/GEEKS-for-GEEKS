0aa467c9d326a130d2c5f466b27eb29cb1d74676
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-03 12:19:10 +0530

    Added solution - LeetHub

---
+++ 700176-merge-two-sorted-linked-lists-GFG/700176-merge-two-sorted-linked-lists.cpp
@@ -0,0 +1,47 @@
+/*
+class Node {
+ public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* sortedMerge(Node* head1, Node* head2) {
+        // code here
+        if (head1 == nullptr) return head2;
+        if (head2 == nullptr) return head1;
+        
+        Node dummy(0);
+        Node* tail = &dummy;
+        
+        while (head1 != nullptr && head2 != nullptr) {
+            if (head1->data <= head2->data) {
+                tail->next = head1;
+                head1 = head1->next;
+            } else {
+                tail->next = head2;
+                head2 = head2->next;
+            }
+            tail = tail->next;
+        }
+        
+        if (head1 != nullptr) {
+            tail->next = head1;
+        } else {
+            tail->next = head2;
+        }
+        
+        return dummy.next;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
