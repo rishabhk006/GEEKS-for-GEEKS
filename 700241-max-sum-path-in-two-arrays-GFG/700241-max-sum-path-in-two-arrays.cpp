beecef75b71abf5fe8f3a88a076427c9709e51a2
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-06 10:27:48 +0530

    Added solution - LeetHub

---
+++ 700241-max-sum-path-in-two-arrays-GFG/700241-max-sum-path-in-two-arrays.cpp
@@ -0,0 +1,42 @@
+class Solution {
+  public:
+    int maxPathSum(vector<int> &a, vector<int> &b) {
+        int i = 0, j = 0;
+        int sum1 = 0, sum2 = 0, ans = 0;
+
+        while (i < a.size() && j < b.size()) {
+            if (a[i] < b[j]) {
+                sum1 += a[i];
+                i++;
+            }
+            else if (a[i] > b[j]) {
+                sum2 += b[j];
+                j++;
+            }
+            else {
+                ans += max(sum1, sum2) + a[i];
+                sum1 = sum2 = 0;
+                i++;
+                j++;
+            }
+        }
+
+        while (i < a.size()) {
+            sum1 += a[i];
+            i++;
+        }
+
+        while (j < b.size()) {
+            sum2 += b[j];
+            j++;
+        }
+
+        ans += max(sum1, sum2);
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
