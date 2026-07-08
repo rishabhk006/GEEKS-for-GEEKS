6da2ae238e4559c4ab583ed735a30f7fce9fe58f
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-08 15:45:13 +0530

    Added solution - LeetHub

---
+++ 709919-reverse-a-stack-GFG/709919-reverse-a-stack.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    void insertAtBottom(stack<int>& st, int element) {
+        if (st.empty()) {
+            st.push(element);
+            return;
+        }
+
+        int topElement = st.top();
+        st.pop();
+        
+        insertAtBottom(st, element);
+        
+        st.push(topElement);
+    }
+
+public:
+    void reverseStack(stack<int> &st) {
+        if (st.empty()) {
+            return;
+        }
+
+        int topElement = st.top();
+        st.pop();
+        
+        reverseStack(st);
+        
+        insertAtBottom(st, topElement);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
