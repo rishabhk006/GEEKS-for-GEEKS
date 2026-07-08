0d7aa2db93558a0fa1ed9a335a81123639bb5b0e
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-08 15:48:45 +0530

    Added solution - LeetHub

---
+++ 705195-longest-valid-parentheses-GFG/705195-longest-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxLength(string& s) {
+        stack<int> st;
+        st.push(-1);
+        int max_len = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                st.push(i);
+            } else {
+                st.pop();
+                if (st.empty()) {
+                    st.push(i);
+                } else {
+                    max_len = max(max_len, i - st.top());
+                }
+            }
+        }
+        return max_len;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
