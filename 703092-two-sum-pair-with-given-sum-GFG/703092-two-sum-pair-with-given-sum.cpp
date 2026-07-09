1a4a3f6ed0ad539805130d71eef2aced44a1bda0
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-09 15:47:57 +0530

    Added solution - LeetHub

---
+++ 703092-two-sum-pair-with-given-sum-GFG/703092-two-sum-pair-with-given-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool twoSum(vector<int>& arr, int target) {
+        unordered_set<int> st;
+
+        for (int num : arr) {
+            if (st.count(target - num))
+                return true;
+
+            st.insert(num);
+        }
+
+        return false;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
