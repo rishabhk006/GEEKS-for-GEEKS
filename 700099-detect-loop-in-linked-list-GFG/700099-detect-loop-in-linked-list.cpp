6795a8a709c6368a12e37bf20c1f99b87aa5fea5
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-09 11:00:40 +0530

    Added solution - LeetHub

---
+++ 700099-detect-loop-in-linked-list-GFG/700099-detect-loop-in-linked-list.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool detectLoop(Node* head) {
+        Node *slow = head, *fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
