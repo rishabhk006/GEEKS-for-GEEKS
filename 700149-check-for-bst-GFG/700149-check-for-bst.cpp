561a210c6ff54813a935caf33c37ee951d636c46
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-09 10:58:01 +0530

    Added solution - LeetHub

---
+++ 700149-check-for-bst-GFG/700149-check-for-bst.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool solve(Node* root, long long low, long long high) {
+        if (root == NULL)
+            return true;
+
+        if (root->data <= low || root->data >= high)
+            return false;
+
+        return solve(root->left, low, root->data) &&
+               solve(root->right, root->data, high);
+    }
+
+    bool isBST(Node* root) {
+        return solve(root, LLONG_MIN, LLONG_MAX);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
