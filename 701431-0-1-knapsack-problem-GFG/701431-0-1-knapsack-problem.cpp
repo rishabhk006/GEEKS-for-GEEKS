cd76d0d84a3003b24aad4230e75752d596f26ccb
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-09 10:58:49 +0530

    Added solution - LeetHub

---
+++ 701431-0-1-knapsack-problem-GFG/701431-0-1-knapsack-problem.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int knapsack(int W, vector<int> &val, vector<int> &wt) {
+        vector<int> dp(W + 1, 0);
+        int n = val.size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = W; j >= wt[i]; j--) {
+                dp[j] = max(dp[j], val[i] + dp[j - wt[i]]);
+            }
+        }
+
+        return dp[W];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
