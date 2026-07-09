5fa112817941e58eb063a326534208d0ad1daaa5
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-09 10:51:49 +0530

    Added solution - LeetHub

---
+++ 706296-count-pairs-divisible-by-k-GFG/706296-count-pairs-divisible-by-k.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long countKdivPairs(vector<int>& arr, int k) {
+        vector<long long> freq(k, 0);
+        
+        for (int x : arr)
+            freq[x % k]++;
+        
+        long long ans = (freq[0] * (freq[0] - 1)) / 2;
+        
+        for (int i = 1; i <= (k - 1) / 2; i++)
+            ans += freq[i] * freq[k - i];
+        
+        if (k % 2 == 0)
+            ans += (freq[k / 2] * (freq[k / 2] - 1)) / 2;
+        
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
