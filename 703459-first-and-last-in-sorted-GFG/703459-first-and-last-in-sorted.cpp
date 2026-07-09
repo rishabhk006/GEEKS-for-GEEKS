29d5faeb42a778356153b27834f12e22aa81b734
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-09 11:01:49 +0530

    Added solution - LeetHub

---
+++ 703459-first-and-last-in-sorted-GFG/703459-first-and-last-in-sorted.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> find(vector<int>& arr, int x) {
+        int first = -1, last = -1;
+        int n = arr.size();
+
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == x) {
+                first = mid;
+                high = mid - 1;
+            } else if (arr[mid] < x) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        low = 0;
+        high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == x) {
+                last = mid;
+                low = mid + 1;
+            } else if (arr[mid] < x) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return {first, last};
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
