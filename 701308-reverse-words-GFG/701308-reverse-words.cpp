06facc9644c84585bd08e8bd70422cf3a7b2982e
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-09 15:49:25 +0530

    Added solution - LeetHub

---
+++ 701308-reverse-words-GFG/701308-reverse-words.cpp
@@ -0,0 +1,38 @@
+class Solution {
+  public:
+    string reverseWords(string &s) {
+        // code here
+         vector<string> words;
+        string temp;
+
+        for (char c : s) {
+            if (c == '.') {
+                if (!temp.empty()) {
+                    words.push_back(temp);
+                    temp.clear();
+                }
+            } else {
+                temp += c;
+            }
+        }
+
+        if (!temp.empty())
+            words.push_back(temp);
+
+        reverse(words.begin(), words.end());
+
+        string ans;
+
+        for (int i = 0; i < words.size(); i++) {
+            ans += words[i];
+            if (i != words.size() - 1)
+                ans += '.';
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
