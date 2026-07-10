e6be2b1c42599143ed4de2e9e0fde596004f1e28
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-10 23:03:20 +0530

    Added solution - LeetHub

---
+++ 704548-ways-to-express-as-sum-of-consecutives-GFG/704548-ways-to-express-as-sum-of-consecutives.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int getCount(int n) {
+        // code here
+        int ans = 0;
+
+        for (int k = 2; k * (k - 1) / 2 < n; k++) {
+            int rem = n - k * (k - 1) / 2;
+
+            if (rem % k == 0)
+                ans++;
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
