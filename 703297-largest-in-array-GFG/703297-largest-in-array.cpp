0da25a0e2091114b9934f227d3626585d05fba73
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-13 10:57:04 +0530

    Added solution - LeetHub

---
+++ 703297-largest-in-array-GFG/703297-largest-in-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+        int mx = arr[0];
+        
+        for (int x : arr)
+            mx = max(mx, x);
+        
+        return mx;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
