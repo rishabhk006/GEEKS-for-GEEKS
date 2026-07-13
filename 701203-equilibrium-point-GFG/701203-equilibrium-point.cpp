654bbeddace6280dbdda1817bc76d9dc552d3f39
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-13 10:55:22 +0530

    Added solution - LeetHub

---
+++ 701203-equilibrium-point-GFG/701203-equilibrium-point.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int findEquilibrium(vector<int> &arr) {
+        long long total = 0;
+        
+        for (int x : arr)
+            total += x;
+        
+        long long left = 0;
+        
+        for (int i = 0; i < arr.size(); i++) {
+            total -= arr[i];
+            
+            if (left == total)
+                return i;
+            
+            left += arr[i];
+        }
+        
+        return -1;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
