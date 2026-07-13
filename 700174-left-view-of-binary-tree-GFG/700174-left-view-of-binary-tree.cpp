0ed3ac371ff77e3ed79d9e51c15749be0a2c1adb
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-13 11:03:55 +0530

    Added solution - LeetHub

---
+++ 700174-left-view-of-binary-tree-GFG/700174-left-view-of-binary-tree.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    vector<int> leftView(Node *root) {
+        vector<int> ans;
+        if (!root) return ans;
+
+        queue<Node*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int sz = q.size();
+
+            for (int i = 0; i < sz; i++) {
+                Node* cur = q.front();
+                q.pop();
+
+                if (i == 0)
+                    ans.push_back(cur->data);
+
+                if (cur->left)
+                    q.push(cur->left);
+
+                if (cur->right)
+                    q.push(cur->right);
+            }
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
