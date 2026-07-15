46bb09f68cc1edb73ecc37832d6f72ff12092750
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-15 11:14:32 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 700391-palindrome-linked-list-GFG/700391-palindrome-linked-list.cpp
@@ -0,0 +1,45 @@
+class Solution {
+  public:
+    Node* reverse(Node* head) {
+        Node* prev = NULL;
+
+        while (head) {
+            Node* next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
+
+    bool isPalindrome(Node *head) {
+        if (!head || !head->next)
+            return true;
+
+        Node *slow = head, *fast = head;
+
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        Node* second = reverse(slow->next);
+        Node* first = head;
+        Node* temp = second;
+
+        while (temp) {
+            if (first->data != temp->data)
+                return false;
+
+            first = first->next;
+            temp = temp->next;
+        }
+
+        return true;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
