1f8ca999c42886cb92b302d68e8f8e2587917d49
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-15 11:29:28 +0530

    Added solution - Time: O(N^2 log N), Space: O(1)

---
+++ 709904-even-swap-GFG/709904-even-swap.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> lexicographicallyLargest(vector<int>& arr, int N) {
+        int i = 0;
+
+        while (i < N) {
+            int j = i;
+            while (j + 1 < N && (arr[j] + arr[j + 1]) % 2 == 0)
+                j++;
+
+            sort(arr.begin() + i, arr.begin() + j + 1, greater<int>());
+
+            i = j + 1;
+        }
+
+        return arr;
+    }
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
