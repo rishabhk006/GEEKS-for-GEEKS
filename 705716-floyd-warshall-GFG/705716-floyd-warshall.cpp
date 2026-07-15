60d7f3ee6098aab329d4bc60274d4c2a70e64594
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-15 11:16:26 +0530

    Added solution - Time: O(n^3), Space: O(1)

---
+++ 705716-floyd-warshall-GFG/705716-floyd-warshall.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    void floydWarshall(vector<vector<int>> &dist) {
+        int n = dist.size();
+        const int INF = 100000000;
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                if (dist[i][k] == INF) continue;
+
+                for (int j = 0; j < n; j++) {
+                    if (dist[k][j] == INF) continue;
+
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
