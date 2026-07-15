9390da162aef3f0d2178fc341305ff0d3c328b8c
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-15 11:11:02 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 701417-stickler-thief-GFG/701417-stickler-thief.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int findMaxSum(vector<int>& arr) {
+        int prev1 = 0, prev2 = 0;
+
+        for (int x : arr) {
+            int cur = max(prev1, prev2 + x);
+            prev2 = prev1;
+            prev1 = cur;
+        }
+
+        return prev1;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
