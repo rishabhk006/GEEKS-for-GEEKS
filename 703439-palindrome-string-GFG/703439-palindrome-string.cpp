d128fb2e2e1711608a678fb49de365345ddcaf37
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-16 15:37:36 +0530

    Force committed solution

---
+++ 703439-palindrome-string-GFG/703439-palindrome-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    bool isPalindrome(string& s) {
+        // code here
+        int left = 0;
+        int right = s.size() - 1;
+        
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
