fde0eaf24290cf18a2c450119118d4cc9c7d43bb
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-20 00:08:25 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 703402-longest-prefix-suffix-GFG/703402-longest-prefix-suffix.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+    int getLPSLength(string &s) {
+        int n = s.length();
+        if (n == 0) return 0;
+        
+        vector<int> lps(n, 0);
+        int len = 0;
+        int i = 1;
+        
+        while (i < n) {
+            if (s[i] == s[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        
+        return lps[n - 1];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
