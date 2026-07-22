3a871122ea25b6c809f9211a9f72475e9c52505a
Rishabh Kumar <149006661+rishabhk006@users.noreply.github.com>
2026-07-22 11:54:57 +0530

    Added solution - Time: O(n log n), Space: O(n)

---
+++ 704021-minimum-deletions-to-make-sorted-GFG/704021-minimum-deletions-to-make-sorted.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int minDeletions(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> tails;
+
+        for (int x : arr) {
+            // Find the first element in tails that is >= x
+            auto it = lower_bound(tails.begin(), tails.end(), x);
+
+            if (it == tails.end()) {
+                // x is strictly greater than all elements in tails
+                tails.push_back(x);
+            } else {
+                // Replace the element to keep tail values as small as possible
+                *it = x;
+            }
+        }
+
+        // LIS length is tails.size()
+        int lisLength = tails.size();
+
+        return n - lisLength;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
